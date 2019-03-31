c57e6173e11a5fc2957e6bf77e6a53b406f09744
Juan Cervantes <juancervantes@Juans-MacBook-Pro.local>
2019-03-31 16:53:37 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Question.create(
+    title: 'Are you interested in Athletics?',
+    options: {
+      'yes' : ['Sports', 'Wellness', 'Outdoors'],
+      'no' : ['Food', 'Indoors']
+    },
+    next_question: {
+      'yes': 2
+      'no': 3
+    }
+    can_skip: false
+  )
+  Question.create(
+    title: 'Which of the following appeals the most to you?',
+    options: {
+      'Video Games' : ['Gaming'],
+      'Hiking' : ['Sports', 'Nature'],
+      'Cooking' : ['Food']
+    },
+    next_question: {
+      'Video Games' : 3
+      'Hiking' : 4
+      'Cooking' : 5
+    },
+    can_skip: true
+  )
+  Question.create(
+    title: 'Are you interested in Greek Life?',
+    options: {
+      'yes' : ['Greek', 'Professional', 'Business'],
+      'no' : ['Food', 'Indoors']
+    },
+    next_question: {
+      'yes': 2
+      'no': 3
+    }
+    can_skip: false
+  )
