8830bb0bfc2105f2facbc28b6b52c222cddd0d2a
Juan Cervantes <juanxcerv@berkeley.edu>
2019-04-13 17:48:28 -0700

    checking test coverage for the landing page and placeholder controller

--- app/assets/javascripts/slicked.js
+++ app/assets/javascripts/slicked.js
@@ -1,36 +1,34 @@
-var $jq = jQuery.noConflict();
-$jq(document).ready(function(){
-  $jq('.orgs').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+$(document).ready(function() {
+  $('.orgs').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
-	
+    ]
+  });
 });
---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,30 @@
+Given /^(?:|I )am on "(.+)"$/ do |page_name|
+  if page_name == "landing page"
+    visit landing_path
+  end
+  if page_name == "home page"
+    visit root_path
+  end
+end
+
+Then /I should see a sign in button/ do
+  expect(find_button("login-button").value).to eq("Placeholder Login")
+end
+
+Then /I should be redirected to "(.*)"$/ do |page_name|
+  if page_name == "landing page"
+    current_path.should == landing_path
+  end
+  if page_name == 'home page'
+    current_path.should == root_path
+  end
+end
+
+And /click sign in button/ do
+  click_button('login-button')
+end
+
+Given /I am logged in/ do
+  visit landing_path
+  click_button('login-button')
+end
