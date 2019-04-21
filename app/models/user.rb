70e535ce84cdf0511fe20a4b1d99fe76c2b0b40c
Eilen Shahbaz <shahbazeilen@gmail.com>
2019-04-20 21:27:56 -0700

    add more info to db upon user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
       user.uid = auth['uid']
       if auth['info']
         user.first_name = auth['info']['name'] || ""
-        user.last_name = auth[]
+        user.last_name = auth['info']['last_name'] || ""
+        user.email = auth['info']["email"] || ""
       end
     end
   end
