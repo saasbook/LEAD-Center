fc81567444a1cc4280fad6b95f348b1ce30f742b
Hekaiyo <hekaiyo@gmail.com>
2019-04-28 17:29:23 -0700

    Cleaned up a bit

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,52 +3,61 @@ require 'json'
 
 class Organization
 
-  attr_accessor :id, :name, :description, :imageUrl, :categories, :interests
+    attr_accessor :id, :name, :description, :imageUrl, :categories, :interests
 
-  def initialize(id, name, description, imageUrl, categories, interests)
-    @id = id
-    @name = name
-    @description = description
-    @imageUrl = imageUrl
-    @categories = categories
-    @interests = interests
-  end
-
-  def self.get_organizations(num, categories, interests)
-    url = 'https://callink.berkeley.edu/api/Organizations?page='
-    totalPages = 1
-    pageNumber = 1
-    orgs = []
-    while pageNumber <= totalPages
-      response = RestClient.get url + pageNumber.to_s
-      parsed = JSON.parse(response)
-      orgs = orgs + parsed['items']
-      totalPages = parsed['totalPages']
-      pageNumber += 1
+    def initialize(id, name, description, imageUrl, categories, interests)
+        @id = id
+        @name = name
+        @description = description
+        @imageUrl = imageUrl
+        @categories = categories
+        @interests = interests
     end
-    top_orgs = []
-    orgs.each do |i|
-        if categories == nil||categories.include?(i['type'])
-            top_orgs.push(Organization.new(i['organizationId'], i['name'], i['description'], i['profileImageUrl'], i['type'], i['categories']))
+
+    def self.get_organizations(num, categories, interests)
+        orgs = self.get_orgs_from_api()
+	
+        top_orgs = []
+        orgs.each do |i|
+            if categories == nil || categories.include?(i['type'])
+                top_orgs.push(Organization.new(i['organizationId'], i['name'], i['description'], i['profileImageUrl'], i['type'], i['categories']))
+            end
         end
-      end
-    if interests != nil
-      top_orgs = top_orgs.sort_by { |org| -org.count_interest(interests)}
+	
+        if interests != nil
+            top_orgs = top_orgs.sort_by { |org| -org.count_interest(interests)}
+        end
+	
+        return top_orgs[0 ... num]
     end
-    top_orgs = top_orgs[0 ... num]
-    return top_orgs
-  end
-
-  def count_interest(i)
-    count = 0
-    self.interests.each do |ctg|
-      if i.include?(ctg['categoryName'])
-        count += 1
-      end
+  
+    def self.get_orgs_from_api()
+        url = 'https://callink.berkeley.edu/api/Organizations'
+        orgs = []
+        pageNumber = 0
+	
+        loop do
+            pageNumber += 1
+            response = RestClient.get url, :params => {:page => pageNumber}
+            parsed = JSON.parse(response)
+            orgs += parsed['items']
+            totalPages = parsed['totalPages']
+            pageNumber = parsed['pageNumber']
+	  
+            break if pageNumber >= totalPages
+        end
+        
+        return orgs
     end
-    return count
-  end
 
-
-
+    def count_interest(i)
+        count = 0
+        self.interests.each do |ctg|
+            if i.include?(ctg['categoryName'])
+                count += 1
+            end
+        end
+        return count
+    end
+    
 end
