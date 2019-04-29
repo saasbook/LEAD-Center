78f35e070bca682d51176921cf560fc97f66477c
Hekaiyo <hekaiyo@gmail.com>
2019-04-28 17:12:31 -0700

    Tony's work

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,23 +3,52 @@ require 'json'
 
 class Organization
 
-  attr_accessor :id, :name, :description, :imageUrl
+  attr_accessor :id, :name, :description, :imageUrl, :categories, :interests
 
-  def initialize(id, name, description, imageUrl)
+  def initialize(id, name, description, imageUrl, categories, interests)
     @id = id
     @name = name
     @description = description
     @imageUrl = imageUrl
+    @categories = categories
+    @interests = interests
   end
 
   def self.get_organizations(num, categories, interests)
-    response = RestClient.get 'https://callink.berkeley.edu/api/Organizations'
-    parsed = JSON.parse(response)
-    orgs = parsed['items']
+    url = 'https://callink.berkeley.edu/api/Organizations?page='
+    totalPages = 1
+    pageNumber = 1
+    orgs = []
+    while pageNumber <= totalPages
+      response = RestClient.get url + pageNumber.to_s
+      parsed = JSON.parse(response)
+      orgs = orgs + parsed['items']
+      totalPages = parsed['totalPages']
+      pageNumber += 1
+    end
     top_orgs = []
-    neworg = Organization.new(orgs[0]['organizationId'], orgs[0]['name'], orgs[0]['description'], orgs[0]['profileImageUrl'])
-    top_orgs.push(neworg)
+    orgs.each do |i|
+        if categories == nil||categories.include?(i['type'])
+            top_orgs.push(Organization.new(i['organizationId'], i['name'], i['description'], i['profileImageUrl'], i['type'], i['categories']))
+        end
+      end
+    if interests != nil
+      top_orgs = top_orgs.sort_by { |org| -org.count_interest(interests)}
+    end
+    top_orgs = top_orgs[0 ... num]
     return top_orgs
   end
+
+  def count_interest(i)
+    count = 0
+    self.interests.each do |ctg|
+      if i.include?(ctg['categoryName'])
+        count += 1
+      end
+    end
+    return count
+  end
+
+
 
 end
